d30e74495056e1e115b921793d79fd2b40007920
Bettsy Iriarte Gutierrez <birigu170299@gmail.com>
2024-06-06 23:40:00 -0300

    reto FrontendMentor

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import IconFylo from "../icons/IconFylo";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <button>
+          <IconFylo />
+        </button>
+
+        <div>
+          <button>Features</button>
+          <button>Team</button>
+          <button>Sign In</button>
+        </div>
+
+      </div>
+
+    </header>
+
+  )
+}
+export default Header;
